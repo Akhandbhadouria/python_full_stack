d80ceb5db045de233e6e90561fd8d4ee77a495d1
Akhandbhadouria <148524224+Akhandbhadouria@users.noreply.github.com>
2026-01-24 18:36:17 +0530

    updated

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_h&47w6u@yb13xm$fimw5#j7^cdx_7h7q94u($g61j0zd=c+kd'
+SECRET_KEY = 'django-insecure-@)u7cuc$o2)j!v(dkj0*@7!h62#40=7yx=zxerb_udd7jj_c%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,23 +74,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student',
+        'NAME': 'django_db',
         'USER': 'root',
-        'PASSWORD': 'password@123',
-        'HOST': 'localhost',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- mysite/test_1/models.py
+++ mysite/test_1/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, null=True, blank=True)
-    password = models.CharField(max_length=100, null=True, blank=True)
-    mobile = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+    reg=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
