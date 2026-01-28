1adc1ca84fe4d17916f04a39320555b652243dd7
Akhandbhadouria <148524224+Akhandbhadouria@users.noreply.github.com>
2026-01-28 11:53:22 +0530

    account_logic

--- djenv/bin/activate.fish
+++ djenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/lambardaar/Desktop/python_full_stack/djenv
+set -gx VIRTUAL_ENV /Users/lambardaar/Downloads/python_full_stack/djenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
